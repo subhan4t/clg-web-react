b6f1456b3474d6493b6613f80853184ffa4a3dfb
subhan4t <saleemsubhan2003@gmail.com>
2024-07-22 13:32:21 +0530

    Delete Saveetha-Site-main directory

--- Saveetha-Site-main/src/components/EventComponent/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const footer = () => (
-    <footer className="footer">
-        <p className="footer-text">© 2024 Our College. All rights reserved.</p>
-    </footer>
-);
-
-export default footer;
